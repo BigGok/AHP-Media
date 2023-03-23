d79788f10678174aa01bb7f20b2fef9ea13dd4c4
Luisnguyen1 <103302594+Luisnguyen1@users.noreply.github.com>
2023-03-23 17:37:29 +0700

    update

--- news-details.php
+++ news-details.php
@@ -36,10 +36,16 @@ endif;
     <title>AHP Agency</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/x-icon" href="/images/ahp icon.png">
     <!--/*font awesome icons*/-->
     <link type="text/css" rel="stylesheet" href="css/font-awesome.min.css" />
     <link rel="stylesheet" href="fontawesome-free-6.2.0-web/css/all.css">
     <!--rubik font family -->
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
     <link
         href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&family=Rubik:ital,wght@0,400;0,700;1,700&display=swap"
         rel="stylesheet">
@@ -318,7 +324,7 @@ endif;
 
     </section>
     <section class = "content-body">
-    <div class="container">
+        <div class="container">
 
 
      
@@ -326,7 +332,7 @@ endif;
 
         <!-- Blog Entries Column -->
         <div class="col-md-8">
-
+        border: 1px solid white
           <!-- Blog Post -->
 <?php
 $pid=intval($_GET['nid']);
@@ -334,7 +340,7 @@ $pid=intval($_GET['nid']);
 while ($row=mysqli_fetch_array($query)) {
 ?>
 
-          <div class="card mb-4">
+          <div class="card mb-4" style="   margin-left: -25px;margin-right: -25px">
       
             <div class="card-body">
               <h2 class="card-title"><?php echo htmlentities($row['posttitle']);?></h2>
@@ -343,14 +349,78 @@ while ($row=mysqli_fetch_array($query)) {
                 <hr />
 
  <img class="img-fluid rounded" src="admin/postimages/<?php echo htmlentities($row['PostImage']);?>" alt="<?php echo htmlentities($row['posttitle']);?>">
- 
-              <p class="card-text"><?php $pt=$row['postdetails'];
+  
+              <p class="card-text"><?php 
+$pt=$row['postdetails'];
               echo  (substr($pt,0));?></p>
              
             </div>
-            
+            <div class="card-footer text-muted">
+             
+           
+            </div>
           </div>
 <?php } ?>
+       
+
+      
+
+     
+
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+      <?php include('includes/sidebar.php');?>
+      </div>
+      <!-- /.row -->
+<!---Comment Section --->
+
+ <div class="row" style="margin-top: -8%">
+   <div class="col-md-8">
+<div class="card my-4">
+            <h5 class="card-header">Leave a Comment:</h5>
+            <div class="card-body">
+              <form name="Comment" method="post">
+      <input type="hidden" name="csrftoken" value="<?php echo htmlentities($_SESSION['token']); ?>" />
+ <div class="form-group">
+<input type="text" name="name" class="form-control" placeholder="Nhập họ và têntên" required>
+</div>
+
+ <div class="form-group">
+ <input type="email" name="email" class="form-control" placeholder="Nhập email của bạnbạn" required>
+ </div>
+
+
+                <div class="form-group">
+                  <textarea class="form-control" name="comment" rows="3" placeholder="Comment" required></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary" name="submit">GửiGửi</button>
+              </form>
+            </div>
+          </div>
+
+  <!---Comment Display Section --->
+
+ <?php 
+ $sts=1;
+ $query=mysqli_query($con,"select name,comment,postingDate from  tblcomments where postId='$pid' and status='$sts'");
+while ($row=mysqli_fetch_array($query)) {
+?>
+<div class="media mb-4">
+            <img class="d-flex mr-3 rounded-circle" src="images/usericon.png" alt="">
+            <div class="media-body">
+              <h5 class="mt-0"><?php echo htmlentities($row['name']);?> <br />
+                  <span style="font-size:11px;"><b>at</b> <?php echo htmlentities($row['postingDate']);?></span>
+            </h5>
+           
+             <?php echo htmlentities($row['comment']);?>            </div>
+          </div>
+<?php } ?>
+
+        </div>
+      </div>
+    </div>
+
 </section>  
     <div class=" bg-hero">
         <div class="container">
