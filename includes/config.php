a8566f18cc88f33a016f1b15790df9ce6b0a7fb7
Luisnguyen1 <103302594+Luisnguyen1@users.noreply.github.com>
2023-03-23 16:37:54 +0700

    update

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','ejtmyl3hvuxw_admin');
-define('DB_PASS' ,'ahp@1234567890');
-define('DB_NAME','ejtmyl3hvuxw_newsportal');
+define('DB_USER','root');
+define('DB_PASS' ,'');
+define('DB_NAME','newsportal');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
