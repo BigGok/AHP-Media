075c6ee0700db3025e6d84e5a8c82d46adcc3e7d
Luisnguyen1 <103302594+Luisnguyen1@users.noreply.github.com>
2023-03-24 14:49:56 +0700

    update

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -44,15 +44,15 @@ while($row=mysqli_fetch_array($query))
             <h5 class="card-header">Recent News</h5>
             <div class="card-body">
                       <ul class="mb-0">
-<?php
+            <?php
 $query=mysqli_query($con,"select tblposts.id as pid,tblposts.PostTitle as posttitle from tblposts left join tblcategory on tblcategory.id=tblposts.CategoryId left join  tblsubcategory on  tblsubcategory.SubCategoryId=tblposts.SubCategoryId limit 8");
 while ($row=mysqli_fetch_array($query)) {
 
 ?>
 
-  <li>
-                      <a href="news-details.php?nid=<?php echo htmlentities($row['pid'])?>"><?php echo htmlentities($row['posttitle']);?></a>
-                    </li>
+                    
+                      <li style ="margin-left: 10px"><a href="news-details.php?nid=<?php echo htmlentities($row['pid'])?>"><?php echo htmlentities($row['posttitle']);?></a> </li>
+                      
             <?php } ?>
           </ul>
             </div>
