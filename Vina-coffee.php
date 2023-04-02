64e3cf903d65bca0dff8ac186198a49c85bf79dc
Luisnguyen1 <103302594+Luisnguyen1@users.noreply.github.com>
2023-04-02 16:04:13 +0700

    update

--- Vina-coffee.php
+++ Vina-coffee.php
@@ -1,57 +1,27 @@
-<?php
-session_start();
-include('includes/config.php');
-//Genrating CSRF Token
-if (empty($_SESSION['token'])) {
-    $_SESSION['token'] = bin2hex(random_bytes(32));
-}
-
-if (isset($_POST['submit'])) {
-    //Verifying CSRF Token
-    if (!empty($_POST['csrftoken'])) {
-        if (hash_equals($_SESSION['token'], $_POST['csrftoken'])) {
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $comment = $_POST['comment'];
-            $postid = intval($_GET['nid']);
-            $st1 = '0';
-            $query = mysqli_query($con, "insert into tblcomments(postId,name,email,comment,status) values('$postid','$name','$email','$comment','$st1')");
-            if ($query) :
-                echo "<script>alert('comment successfully submit. Comment will be display after admin review ');</script>";
-                unset($_SESSION['token']);
-            else :
-                echo "<script>alert('Something went wrong. Please try again.');</script>";
-
-            endif;
-        }
-    }
-}
-?>
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>AHP Agency</title>
+    <link rel="icon" type="image/x-icon" href="/images/ahp icon.png">
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" type="image/x-icon" href="/images/ahp icon.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=K2D&family=Lalezar&display=swap" rel="stylesheet"> 
     <!--/*font awesome icons*/-->
     <link type="text/css" rel="stylesheet" href="css/font-awesome.min.css" />
     <link rel="stylesheet" href="fontawesome-free-6.2.0-web/css/all.css">
     <!--rubik font family -->
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/modern-business.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&family=Rubik:ital,wght@0,400;0,700;1,700&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&family=Rubik:ital,wght@0,400;0,700;1,700&display=swap"
+        rel="stylesheet">
     <!--custom css-->
-    <link type="text/css" rel="stylesheet" href="css/style.css" />
-    <link href="css/bootstrap.min.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
-    <link href="css/news.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+    <link href="css/style.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
 
     <!--media css-->
-    <link type="text/css" rel="stylesheet" href="css/media.css" />
+    <link href="css/media.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+    
 
     <!--jquery ui files-->
     <link type="text/css" rel="stylesheet" href="css/jquery-ui.structure.min.css" />
@@ -61,30 +31,60 @@ if (isset($_POST['submit'])) {
     <link type="text/css" rel="stylesheet" href="css/owl.carousel.min.css" />
     <link type="text/css" rel="stylesheet" href="css/owl.theme.default.min.css" />
     <link rel="stylesheet" href="scss/style.scss">
+    <link href="css/bootstrap.min.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
     <script type="text/javascript" src="js/vanilla-tilt.js"></script>
-    <script src="https://kit.fontawesome.com/36fcf59f66.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:100,200,300,400,500,600,700,800,900">
 </head>
 
 <body>
-<?php include 'header.php' ?>
-    <!-- END MAIL-BOX -->
-    <div class="container">
-            <?php
-            $pid = intval($_GET['nid']);
-            $query = mysqli_query($con, "select tblposts.PostTitle as posttitle,tblposts.PostImage,tblcategory.CategoryName as category,tblcategory.id as cid,tblsubcategory.Subcategory as subcategory,tblposts.PostDetails as postdetails,tblposts.PostingDate as postingdate,tblposts.PostUrl as url from tblposts left join tblcategory on tblcategory.id=tblposts.CategoryId left join  tblsubcategory on  tblsubcategory.SubCategoryId=tblposts.SubCategoryId where tblposts.id='$pid'");
-            while ($row = mysqli_fetch_array($query)) {
-            ?>
-                        <h2 class="card-title"><?php echo htmlentities($row['posttitle']); ?></h2>
-                        <hr />
+    <?php include 'header.php' ?>
 
-                        <div class="card-pic"><?php
-                                            $pt = $row['postdetails'];
-                                            echo (substr($pt, 0)); ?>
-                        </div>
-                        
-            <?php } ?>
-    </div>
+    <section class="header-bg-parent">
+        <div class="header-col1">
+
+     
+            </div>
+        </div>
+        <!--header-col1-->
+        <div class="swiper1 Swiper">
+            
+            <div class="swiper-pagination"></div>
+        </div>
+
+      
+        <!--header-col2-->
+        <div class="clear"></div>
+        <!--clear-->
+
+    </section>
+
+    <section class="container">
+        <div class="emi">
+            <img class="imgzn" src="images/Vina coffee/1.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/2.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/3.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/4.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/5.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/6.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/7.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/8.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/9.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/10.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/11.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/12.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/13.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/14.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/15.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/16.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/17.jpg" alt="">
+            <img class="imgzn" src="images/Vina coffee/18.jpg" alt="">
+       
+        </div>  
+    </section>
+   
     <div class=" bg-hero">
         <div class="container">
             <div class="hero-footer">
@@ -92,7 +92,7 @@ if (isset($_POST['submit'])) {
                     <h4 style="color: #fff;">ĐĂNG KÍ NHẬN THÔNG TIN TỪ AHP MEDIA</h4>
                 </div>
                 <div class="items-footer2">
-                    <form action="">
+                    <form method="POST" action="email-script.php">
                         <input type="text" placeholder="Họ Tên" name="" id="" class="footer-frm-name">
                         <input type="text" placeholder="Email" name="" id="" class="footer-frm-email">
                         <input type="submit" value="Đăng kí nhận thông tin" class="footer-frm-submit">
@@ -101,7 +101,6 @@ if (isset($_POST['submit'])) {
             </div>
         </div>
     </div>
-
     <footer class="footer-all">
         <div class="container-footer">
             <div class="footer-ct">
@@ -207,31 +206,85 @@ if (isset($_POST['submit'])) {
     <script src="js/pop.js"></script>
     <script src="js/translate.js"></script>
     <script src='//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'></script>
-    <!-- Swiper JS -->
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
 
     <!-- Initialize Swiper -->
     <script src="js/swiper.js"></script>
-    <script type="text/javascript" src="js/vanilla-tilt.js"></script>
 
     <script>
-        VanillaTilt.init(document.querySelectorAll(".img-project"), {
-            max: 1,
-            speed: 900,
-            glare: true,
-            "max-glare": 1,
-        });
-    </script>
-
-</body>
-<script>
-    function toggleDropdown(id) {
-        var dropdown = document.getElementById(id);
-        if (dropdown.style.display === "none") {
-            dropdown.style.display = "block";
-        } else {
-            dropdown.style.display = "none";
+        if (window.location.hostname.substr(0, 4) !== 'www.') {
+            window.location.replace('http://www.' + window.location.hostname + window.location.pathname + window.location.hash);
         }
-    }
-</script>
+
+    </script>
+    <style>
+        .switch {
+    position: relative;
+    display: inline-block;
+    width: 50px;
+    height: 24px;
+  }
+  
+  .switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+  
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
+  
+  .slider:before {
+    position: absolute;
+      content:"";
+    height: 20px;
+    width: 20px;
+    left: 3px;
+    bottom: 3px;
+    background-image: url("images/viet.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    -webkit-transition: .4s;
+    transition: .4s;
+    
+  }
+  
+  input:checked + .slider {
+    background-color: #baadab;
+     
+  }
+  
+  input:focus + .slider {
+    box-shadow: 0 0 1px #2196F3;
+  }
+  
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+    background-image: url("images/eng.png");
+    
+  
+  }
+  
+  .slider.round {
+    border-radius: 30px;
+  }
+  
+  .slider.round:before {
+    border-radius: 50%;
+    
+  }
+    </style>
+</body>
+
 </html>
