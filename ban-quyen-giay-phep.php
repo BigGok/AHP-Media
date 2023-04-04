5ce816028b204cb55d55afb0cd688e398c8bed8f
BigGok <ndtai0912@gmail.com>
2023-04-04 20:25:10 +0700

    up

--- ban-quyen-giay-phep.php
+++ ban-quyen-giay-phep.php
@@ -6,18 +6,20 @@
     <link rel="icon" type="image/x-icon" href="/images/ahp icon.png">
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=K2D&family=Lalezar&display=swap" rel="stylesheet">
     <!--/*font awesome icons*/-->
     <link type="text/css" rel="stylesheet" href="css/font-awesome.min.css" />
     <link rel="stylesheet" href="fontawesome-free-6.2.0-web/css/all.css">
     <!--rubik font family -->
-    <link
-        href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&family=Rubik:ital,wght@0,400;0,700;1,700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&family=Rubik:ital,wght@0,400;0,700;1,700&display=swap" rel="stylesheet">
     <!--custom css-->
-    <link type="text/css" rel="stylesheet" href="css/style.css" />
+    <link href="css/style.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
 
     <!--media css-->
-    <link type="text/css" rel="stylesheet" href="css/media.css" />
+    <link href="css/media.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+
 
     <!--jquery ui files-->
     <link type="text/css" rel="stylesheet" href="css/jquery-ui.structure.min.css" />
@@ -27,100 +29,178 @@
     <link type="text/css" rel="stylesheet" href="css/owl.carousel.min.css" />
     <link type="text/css" rel="stylesheet" href="css/owl.theme.default.min.css" />
     <link rel="stylesheet" href="scss/style.scss">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link href="css/bootstrap.min.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
+    <script type="text/javascript" src="js/vanilla-tilt.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:100,200,300,400,500,600,700,800,900">
 </head>
 
 <body>
-    <?php include 'header.php' ?>
     <section class="header-bg-parent">
-        <div class="header-col1">
-
-     
-            </div>
-        </div>
-        <!--header-col1-->
+        <div class="header-col1"></div>
         <div class="swiper1 Swiper">
-            
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <picture>
+                        <source media="(max-width: 576px)" srcset="images/hinh2.jpg">
+                        <img class="slider-bar" src="images/Banner2.jpg" alt="">
+                    </picture>
+                </div>
+            </div>
             <div class="swiper-pagination"></div>
         </div>
-
-      
-        <!--header-col2-->
         <div class="clear"></div>
-        <!--clear-->
-
     </section>
-
-    <section class="container ">
+    <?php include 'header.php' ?>
+    <section class="container">
         <div class="head-service">
-            <h3>Thiết kế thương hiệu</h3>
+            <h3>DỰ ÁN ĐÃ THỰC HIỆN</h3>
         </div>
-        <div class="hero-service">
-            <div class="items-service it0">
-                <img class="img-service" src="images/dichvutieubieu/tuvan.jpg" alt="">
-                <h3>Chiến lượng thương hiệu</h3>
-                <div class="list-service">
-                    <ul class="items-list-service">
+        <!-- slide-1 -->
+        <div id="slide1" class="contain-blk">
+            <div class="hero-project">
+                <div class="items-project">
+                    <div data-tilt data-tilt-glare data-tilt-max-glare="0.7">
+                        <img class="img-project" src="images/ảnh phần dự án đã thực hiện La Siesta.jpg" alt="">
+                    </div>
+                    <div class="items-pa">
+                        <a href="./la-sieta.html">
+                            <h4>RESORT & SPA LA SIESTA</h4>
+                        </a>
+                        <p>Nhận diện</p>
+                    </div>
 
-                        <img class="img-service2" src="images/dichvutieubieu/tuvan.jpg" alt="">
-                    </ul>
                 </div>
+                <div class="items-project">
+                    <div data-tilt data-tilt-glare data-tilt-max-glare="0.8">
+                        <img class="img-project" src="images/Zony peanut/Ảnh con mục dự án Tony Peanut.jpg" alt="">
+                    </div>
+                    <div class="items-pa">
+                        <a href="./zony-peanut.html">
+                            <h4>TONY PEANUT</h4>
+                        </a>
+                        <p>Nhận diện, bao bì, quảng cáo</p>
+                    </div>
+                </div>
+                <div class="items-project">
+                    <div data-tilt data-tilt-glare data-tilt-max-glare="0.8">
+                        <img class="img-project" src="images/Vina coffee/Ảnh con mục dự án.jpg" alt="">
+                    </div>
+                    <div class="items-pa">
+                        <a href="./Vina-coffee.html">
+                            <h4>VINA COFFEE</h4>
+                        </a>
+                        <p>Bao bì, bộ quà tặng</p>
+                    </div>
+                </div>
+                <div class="items-project">
+                    <div data-tilt data-tilt-glare data-tilt-max-glare="0.8">
+                        <img class="img-project" src="images/Ken-mum/Ảnh con mục dự án ken mum.jpg" alt="">
+                    </div>
+                    <div class="items-pa">
+                        <a href="/Ken-mum.html">
+                            <h4>KEN'S MUM</h4>
+                        </a>
+                        <p>NHẬN DIỆN</p>
+                    </div>
+                </div>
+                <!--
+                <div class="items-project">
+                    <div data-tilt data-tilt-glare data-tilt-max-glare="0.8">
+                        <img class="img-project" src="images/ảnh phần dự án đã thực hiện La Siesta.jpg" alt="">
+                    </div>
+                    <div class="items-pa">
+                        <a href="./du-an-la-siesta.html">
+                            <h4>THIẾT KẾ THƯƠNG HIỆU LA SIESTA</h4>
+                        </a>
+                        <p>NHẬN DIỆN</p>
+                    </div>
+                </div>
+                <div class="items-project">
+                    <div data-tilt data-tilt-glare data-tilt-max-glare="0.8">
+                        <img class="img-project" src="images/ảnh phần dự án đã thực hiện La Siesta.jpg" alt="">
+                    </div>
+                    <div class="items-pa">
+                        <a href="/du-an-la-siesta.html">
+                            <h4>THIẾT KẾ THƯƠNG HIỆU LA SIESTA</h4>
+                        </a>
+                        <p>NHẬN DIỆN</p>
+                    </div>
+                </div>
+                <div class="items-project">
+                    <div data-tilt data-tilt-glare data-tilt-max-glare="0.8">
+                        <img class="img-project" src="images/ảnh phần dự án đã thực hiện La Siesta.jpg" alt="">
+                    </div>
+                    <div class="items-pa">
+                        <a href="/du-an-la-siesta.html">
+                            <h4>THIẾT KẾ THƯƠNG HIỆU LA SIESTA</h4>
+                        </a>
+                        <p>NHẬN DIỆN</p>
+                    </div>
+                </div>
+            -->
             </div>
-            <div class="items-service it1">
-                <img class="img-service" src="images/dichvutieubieu/thietkethuonghieu.jpg" alt="">
-                <h3>Ra mắt thương hiệu</h3>
-                <div class="list-service">
-                    <ul class="items-list-service">
-                        <img class="img-service2" src="images/dichvutieubieu/thietkethuonghieu.jpg" alt="">
-                    </ul>
-                </div>
-            </div>
-        
-          <div class="items-service it2">
-                <img class="img-service" src="images/dichvutieubieu/congngheso.jpg" alt="">
-                <h3>Mở rộng thương hiệu</h3>
-                <div class="list-service">
-                    <ul class="items-list-service">
-                        <img class="img-service2" src="images/dichvutieubieu/congngheso.jpg" alt="">
-                    </ul>
-                </div>
-            </div>
-            <div class="items-service it3">
-                <img class="img-service" src="images/dichvutieubieu/thietkebaobi.jpg" alt="">
-                <h3>Tái định vị thương hiệu</h3>
-                <div class="list-service">
-                    <ul class="items-list-service">
-                        <img class="img-service2" src="images/dichvutieubieu/thietkebaobi.jpg" alt="">
-                    </ul>
-                </div>
-            </div>
-            
-            <div class="items-service it4">
-                <img class="img-service" src="images/dichvutieubieu/banquyen.jpg" alt="">
-                <h3>Quy chế thương hiệu</h3>
-                <div class="list-service">
-                    <ul class="items-list-service">
-                        <img class="img-service2" src="images/dichvutieubieu/banquyen.jpg" alt="">
-                    </ul>
-                </div>
-            </div>
-            
-            <div class="items-service it5">
-                <img class="img-service" src="images/dichvutieubieu/truyenthong.jpg" alt="">
-                <h3>Định giá thương hiệu</h3>
-                <div class="list-service">
-                    <ul class="items-list-service">
-                        <img class="img-service2" src="images/dichvutieubieu/truyenthong.jpg" alt="">
-                    </ul>
-                </div>
-            </div>            
         </div>
-        
+        <!-- slide-2 -->
+        <div id="slide2" class="contain-blk" style="display: none;">
+            <div class="hero-project">
+                <div class="items-project">
+                    <div data-tilt data-tilt-glare data-tilt-max-glare="0.7">
+                        <img class="img-project" src="images/homecontact-bg.jpg" alt="">
+                    </div>
+                    <a href="/du-an-la-siesta.html">
+                        <h4>THIẾT KẾ THƯƠNG HIỆU LA SIESTA</h4>
+                    </a>
+                    <p>NHẬN DIỆN</p>
+                </div>
+            </div>
+        </div>
+        <!-- slide-3 -->
+        <div id="slide3" class="contain-blk" style="display: none;">
+            <div class="hero-project">
+                <div class="items-project">
+                    <div data-tilt data-tilt-glare data-tilt-max-glare="0.7">
+                        <img class="img-project" src="images/ảnh phần dự án đã thực hiện La Siesta.jpg" alt="">
+                    </div>
+                    <a href="/du-an-la-siesta.html">
+                        <h4>THIẾT KẾ THƯƠNG HIỆU LA SIESTA</h4>
+                    </a>
+                    <p>NHẬN DIỆN</p>
+                </div>
+            </div>
+        </div>
+        <!-- slide 4 -->
+        <div id="slide4" class="contain-blk" style="display: none;">
+            <div class="hero-project">
+                <div class="items-project">
+                    <div data-tilt data-tilt-glare data-tilt-max-glare="0.8">
+                        <img class="img-project" src="images/ảnh phần dự án đã thực hiện La Siesta.jpg" alt="">
+                    </div>
+                    <h4>VECO</h4>
+                    <p>NHẬN DIỆN</p>
+                </div>
+            </div>
+        </div>
+        <!-- slide 5 -->
+        <div id="slide5" class="contain-blk" style="display: none;">
+            <div class="hero-project">
+                <div class="items-project">
+                    <div data-tilt data-tilt-glare data-tilt-max-glare="0.8">
+                        <img class="img-project" src="images/ảnh phần dự án đã thực hiện La Siesta.jpg" alt="">
+                    </div>
+                    <h4>VECO</h4>
+                    <p>NHẬN DIỆN</p>
+                </div>
+            </div>
+        </div>
     </section>
-   
+    <!-- END PROJECT -->
+
+
     <?php include 'footer.php' ?>
+
     <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
     <Script src="js/jquery-ui.min.js"></Script>
     <script src="https://rawgit.com/kottenator/jquery-circle-progress/1.2.2/dist/circle-progress.js"></script>
@@ -142,76 +222,7 @@
         if (window.location.hostname.substr(0, 4) !== 'www.') {
             window.location.replace('http://www.' + window.location.hostname + window.location.pathname + window.location.hash);
         }
-
     </script>
-    <style>
-        .switch {
-    position: relative;
-    display: inline-block;
-    width: 50px;
-    height: 24px;
-  }
-  
-  .switch input {
-    opacity: 0;
-    width: 0;
-    height: 0;
-  }
-  
-  .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    -webkit-transition: .4s;
-    transition: .4s;
-  }
-  
-  .slider:before {
-    position: absolute;
-      content:"";
-    height: 20px;
-    width: 20px;
-    left: 3px;
-    bottom: 3px;
-    background-image: url("images/viet.png");
-    background-size: contain;
-    background-repeat: no-repeat;
-    -webkit-transition: .4s;
-    transition: .4s;
-    
-  }
-  
-  input:checked + .slider {
-    background-color: #baadab;
-     
-  }
-  
-  input:focus + .slider {
-    box-shadow: 0 0 1px #2196F3;
-  }
-  
-  input:checked + .slider:before {
-    -webkit-transform: translateX(26px);
-    -ms-transform: translateX(26px);
-    transform: translateX(26px);
-    background-image: url("images/eng.png");
-    
-  
-  }
-  
-  .slider.round {
-    border-radius: 30px;
-  }
-  
-  .slider.round:before {
-    border-radius: 50%;
-    
-  }
-    </style>
 </body>
 
 </html>
--- giay-tieu-de.php
+++ giay-tieu-de.php
@@ -7,21 +7,19 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=K2D&family=Lalezar&display=swap" rel="stylesheet"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=K2D&family=Lalezar&display=swap" rel="stylesheet">
     <!--/*font awesome icons*/-->
     <link type="text/css" rel="stylesheet" href="css/font-awesome.min.css" />
     <link rel="stylesheet" href="fontawesome-free-6.2.0-web/css/all.css">
     <!--rubik font family -->
-    <link
-        href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&family=Rubik:ital,wght@0,400;0,700;1,700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;800&family=Rubik:ital,wght@0,400;0,700;1,700&display=swap" rel="stylesheet">
     <!--custom css-->
     <link href="css/style.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
 
     <!--media css-->
     <link href="css/media.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
-    
+
 
     <!--jquery ui files-->
     <link type="text/css" rel="stylesheet" href="css/jquery-ui.structure.min.css" />
@@ -40,18 +38,25 @@
 </head>
 
 <body>
+    <section class="header-bg-parent">
+        <div class="header-col1"></div>
+        <div class="swiper1 Swiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <picture>
+                        <source media="(max-width: 576px)" srcset="images/hinh2.jpg">
+                        <img class="slider-bar" src="images/Banner2.jpg" alt="">
+                    </picture>
+                </div>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+        <div class="clear"></div>
+    </section>
     <?php include 'header.php' ?>
-
     <section class="container">
         <div class="head-service">
             <h3>DỰ ÁN ĐÃ THỰC HIỆN</h3>
-        </div>
-        <div class="js-project">
-            <li><button id="btn-pr1" class="btn-project">THƯƠNG HIỆU</button> </li>
-            <li><button id="btn-pr1" class="btn-project">BAO BÌ</button> </li>
-            <li><button id="btn-pr1" class="btn-project">QUẢNG CÁO</button> </li>
-            <li><button id="btn-pr1" class="btn-project">MARKETING</button> </li>
-            <li><button id="btn-pr1" class="btn-project">WEBSITE</button> </li>
         </div>
         <!-- slide-1 -->
         <div id="slide1" class="contain-blk">
@@ -192,100 +197,9 @@
         </div>
     </section>
     <!-- END PROJECT -->
-   
-    
-    <footer class="footer-all">
-        <div class="container-footer">
-            <div class="footer-ct">
-                <div class="contain-col1">
-                    <a href="index.html"><img src="images/logo AHP Agency trắng.png" class="logo-footer"></a>
-                    <div class="loc-footer">
-                        <p> <i class="fa-solid fa-envelope"></i> <span class="tel-footer">ahpmedia@ahpvn.com</span></p>
-                        <p> <i class="fa fa-mobile-phone"></i></i><span class="email-footer">
-                                096.88.68.227</span></p>
-                        <p> <i class="fa-solid fa-location-dot"></i><span class="location-footer"> Lầu 3, Số
-                                666/10/1-3-5, Đường 3/2, Phường 14, Quận
-                                10, TP.HCM.</span></p>
-                    </div>
-                </div>
-                <div class="contain-col2">
-                    <div class="foot-cl2">
-                        <div class="footer-rows">
-                            <h3>Về chúng tôi </h3>
-                            <button onclick="toggleDropdown('dropdown-content-1')"><i
-                                    class="fa-solid fa-caret-down"></i></button>
-                        </div>
-                        <div id="dropdown-content-1" class="dropdown-content">
-                            <p>Giới thiệu AHP</p>
-                            <p>Văn hóa AHP</p>
-                            <p>Tuyển dụng</p>
-                            <p>Đội ngũ nhân sự</p>
-                            <p>Khách hàng của AHP</p>
-                            <p>Bản tin của AHP</p>
-                            <p>Hồ sơ năng lực</p>
-                        </div>
-                    </div>
-                    <div class="foot-cl2">
-                        <div class="footer-rows">
-                            <h3>Dịch vụ cung cấp </h3>
-                            <button onclick="toggleDropdown('dropdown-content-2')"><i
-                                    class="fa-solid fa-caret-down"></i></button>
-                        </div>
-                        <div id="dropdown-content-2" class="dropdown-content">
-                            <p>Tư vấn thương hiệu</p>
-                            <p>Thiết kế thương hiệu</p>
-                            <p>Nhận diện thương hiệu</p>
-                            <p>Thương hiệu mới</p>
-                            <p>Kế hoạch truyền thông</p>
-                            <p>Digital marketing</p>
-                        </div>
-                    </div>
-                    <div class="foot-cl2">
-                        <div class="footer-rows">
-                            <h3>Dự án đã thực hiện </h3><button onclick="toggleDropdown('dropdown-content-3')"><i
-                                    class="fa-solid fa-caret-down"></i></button>
-                        </div>
-                        <div id="dropdown-content-3" class="dropdown-content">
-                            <p>Logo</p>
-                            <p>Đặt tên/slogan</p>
-                            <p>Web/app</p>
-                            <p>Bao bì</p>
-                            <p>Calatogue/Profile</p>
-                            <p>Calatogue/Profile</p>
-                        </div>
-                    </div>
-                    <div class="foot-cl2">
-                        <div class="footer-rows">
-                            <h3>Đơn vị thành viên </h3>
-                            <button onclick="toggleDropdown('dropdown-content-4')"><i
-                                    class="fa-solid fa-caret-down"></i></button>
-                        </div>
-                        <div id="dropdown-content-4" class="dropdown-content">
-                            <p>AHP Digital</p>
-                            <p>AHP Pakaging</p>
-                            <p>AHP Academy</p>
-                            <p>AHP Brandgifts</p>
-                            <p>LogoArt</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    </footer>
-    <div class="ban-footer">
-        <div class="container">
-            <div class="social-icons-footer">
-                <h4>Theo dõi chúng tôi</h4>
-                <div>
-                    <a href="https://www.facebook.com/profile.php?id=100090142604061"><i
-                            class="fa-brands fa-facebook-f fb "></i></a>
-                    <a href="https://www.tiktok.com/@ahp.tv?is_from_webapp=1&sender_device=pc"><i
-                            class="fa-brands fa-tiktok fb icon-fl"></i></a>
-                    <a href=""><i class="fa-brands fa-youtube fb icon-fl"></i></a>
-                    <a href=""><i class="fa-brands fa-instagram"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
+
+
+    <?php include 'footer.php' ?>
 
     <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
     <Script src="js/jquery-ui.min.js"></Script>
@@ -308,76 +222,7 @@
         if (window.location.hostname.substr(0, 4) !== 'www.') {
             window.location.replace('http://www.' + window.location.hostname + window.location.pathname + window.location.hash);
         }
-
     </script>
-    <style>
-        .switch {
-    position: relative;
-    display: inline-block;
-    width: 50px;
-    height: 24px;
-  }
-  
-  .switch input {
-    opacity: 0;
-    width: 0;
-    height: 0;
-  }
-  
-  .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    -webkit-transition: .4s;
-    transition: .4s;
-  }
-  
-  .slider:before {
-    position: absolute;
-      content:"";
-    height: 20px;
-    width: 20px;
-    left: 3px;
-    bottom: 3px;
-    background-image: url("images/viet.png");
-    background-size: contain;
-    background-repeat: no-repeat;
-    -webkit-transition: .4s;
-    transition: .4s;
-    
-  }
-  
-  input:checked + .slider {
-    background-color: #baadab;
-     
-  }
-  
-  input:focus + .slider {
-    box-shadow: 0 0 1px #2196F3;
-  }
-  
-  input:checked + .slider:before {
-    -webkit-transform: translateX(26px);
-    -ms-transform: translateX(26px);
-    transform: translateX(26px);
-    background-image: url("images/eng.png");
-    
-  
-  }
-  
-  .slider.round {
-    border-radius: 30px;
-  }
-  
-  .slider.round:before {
-    border-radius: 50%;
-    
-  }
-    </style>
 </body>
 
 </html>
