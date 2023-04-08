161c103691e672cd1b0d37c58d828ab1a079df0c
Luisnguyen1 <103302594+Luisnguyen1@users.noreply.github.com>
2023-04-08 14:39:14 +0700

    update

--- zalo-send.php
+++ zalo-send.php
@@ -3,6 +3,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Zalo\Zalo;
 use Zalo\Zalo\ZaloMessageApi;
+use Zalo\Builder\MessageBuilder;
+use Zalo\ZaloEndpoint;  
 
 $oaId = '1758951227414538507';
 
@@ -17,28 +19,33 @@ $zalo = new Zalo($config);
 
 
 // Lấy access token từ OAuth 2.0
-$accessToken = 'q5PyUD6R7nZHMtrwtveS5D5JVN3SXLCUj3P58zINI4EL5dajvxXSAEaCNHddc7e8WJXoDF-NLoUC0qiRyw5b0VrqI3kbssmxv5a3Bgx4C4oXKZvlxTaJJurlVLtXmqvaZbn-L_J6Hsc8R5bLuELgKh5hIrA_mtPIwbT9Ge7WQ7F0R4zwbkqrQEPz10VjiYatcYyC2UQqCm2U6GyKm8mwExC15HNYWJesj382DUAGEJpgBYeDYPmh3CSh7IsRe0CosXLWKjg8RqcWCW1UrlzlGzq_TdUddKju-5HUORl-H0JsC5aUdvvHETyAG2k7WbaGzIraAVssR0g1EmziEVXNLjs061y';
+$accessToken = 'PNL11GRhEc5aU4072EevRMHmG0qBzKToPXn04bE6Eo8vRna61AfY7MP3Qdviq5HF0naiLZtdB74W8IKIABeuCmae5ZHhkczNNaiMIrhN0sbxIW0A4AaPCGP37Gmlt60PHIrLFLRFN0mrDqSj8CjiIWqbOMaaztfJ9H1uO0VJVMiR0bfzBfv6PJ5zI70Il6X3F49aHXQT7N80LGTj3yq3KXjK8dbrYLuTPaaYV7Ua05rQK1r1OhGDVs1s9HHukZvrQoCm7bNHE3bx5JrT5TW5M3mW958hkIHm160uK1QXH4adPqT-DuvEMozcJ3WIWrGX3NrMEWQSMXzfH50-0enTErkhJr8Ay3rc';
 
-// Tạo đối tượng Zalo Message API
-$zaloMessageApi = new ZaloMessageApi($accessToken);
-
-// Lấy thông tin người dùng từ số điện thoại
-$phoneNumber = '0392737834';
-$userProfile = $zaloMessageApi->getUserProfile($phoneNumber);
-
-// Gửi tin nhắn đến người dùng
-$fromId = $oaId;
-$toId = $userProfile['data']['user_id'];
-$message = 'Xin chào, đây là tin nhắn test từ Zalo Message API!';
-$attachment = null;
-$result = $zaloMessageApi->sendTextMessage($fromId, $toId, $message, $attachment);
-
-// Kiểm tra kết quả
-if ($result['error_code'] == 0) {
-    echo 'Đã gửi tin nhắn thành công!';
-} else {
-    echo 'Gửi tin nhắn thất bại: ' . $result['error_message'];
-}
+// build data
+$msgBuilder = new MessageBuilder('text');
+$msgBuilder->withUserId('494021888309207992');
+$msgBuilder->withText('Message Text');
+ 
+// add buttons (only support 5 buttons - optional)
+$actionOpenUrl = $msgBuilder->buildActionOpenURL('https://wwww.google.com'); // build action open link
+$msgBuilder->withButton('Open Link', $actionOpenUrl);
+ 
+$actionQueryShow = $msgBuilder->buildActionQueryShow('query_show'); // build action query show
+$msgBuilder->withButton('Query Show', $actionQueryShow);
+ 
+$actionQueryHide = $msgBuilder->buildActionQueryHide('query_hide'); // build action query hide
+$msgBuilder->withButton('Query Hide', $actionQueryHide);
+ 
+$actionOpenPhone = $msgBuilder->buildActionOpenPhone('0376614498'); // build action open phone
+$msgBuilder->withButton('Open Phone', $actionOpenPhone);
+ 
+$actionOpenSMS = $msgBuilder->buildActionOpenSMS('0376614498', 'sms text'); // build action open sms
+$msgBuilder->withButton('Open SMS', $actionOpenSMS);
+ 
+$msgText = $msgBuilder->build();
+// send request
+$response = $zalo->post(ZaloEndpoint::API_OA_SEND_MESSAGE, $accessToken, $msgText);
+$result = $response->getDecodedBody(); // result
 
 
 ?>
